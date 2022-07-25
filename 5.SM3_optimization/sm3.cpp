0de65e945138fc5dd56c37414c809dfdfdd0b804
Lunan <834946595@qq.com>
2022-07-26 00:14:40 +0800

    Create sm3.cpp

---
+++ 5.SM3_optimization/sm3.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
+#include"sm3.h"
+
+#define u_char unsigned char
+
+
+
+using namespace std;
+
+
+int main()
+{
+	int batch;
+	cout << "请输入测试规模：";
+	cin >> batch;
+
+	clock_t begin, end;
+	string msg = "hello world";
+	u_char* c_msg = (u_char*)msg.c_str();
+	unsigned char res[32];
+	
+	begin = clock();
+	for (int i = 0; i < batch; i++) {
+		sm3(c_msg, msg.size(), res);
+	}
+	end = clock();
+	cout << "测试次数：" << batch << endl;
+	cout << "效率用时：" << double(end - begin) / CLOCKS_PER_SEC << "s" << endl;
+
+	return 0;
+}
